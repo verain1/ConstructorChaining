061b0ce1295d9720ca7ed972325e8686961112f7
Verain <verainthakur1183@gmail.com>
2021-12-30 21:20:13 +0530

    Constructor Chaining

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace staticCons
+{
+    class Hcl
+    {
+        static Hcl()
+        {
+            System.Console.WriteLine("Hcl static constructor");
+        }
+        public Hcl()
+        {
+            System.Console.WriteLine("Hcl default constructor");
+        }
+
+        public Hcl(string newsPaper)
+        {
+            System.Console.WriteLine($"{newsPaper}");
+        }
+    }
+
+    class Employee: Hcl
+    {
+        public Employee(string np): base(np)
+        {
+
+        }
+        public static void Main()
+        {
+            Employee verain = new Employee("Times of India");
+        }
+
+    }
+}
---
+++ readMe.txt
@@ -0,0 +1,10 @@
+*What about the base class parameterized constructor?
+Sol: 
+To call the base class parameterized constructor in inheritance mechanism,
+we have to apply constructor chaining otherwise base class constructor 
+will not be called in any situation.
+
+*To Implement constructor chaining in inheritance, base is a keyword.
+
+*To Implement constructor chaining in inheritance, derived class has 
+to have the (parameterized constructor or Default constructor).
